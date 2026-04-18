48a0b041adb8d2ac0cc76cc053c1ac210f9b4c42
AIP-Code <aiprojectxd@gmail.com>
2026-04-18 15:05:50 +0700

    Add Roadmap

---
+++ src/features/landing/components/RoadmapSection.jsx
@@ -0,0 +1,17 @@
+function RoadmapSection() {
+    return(
+        <section className="justify-center items-center">
+                <div className="pl-22 pt-10 flex justify-center items-center gap-10">
+                    <h2 className="text-5xl font-semibold tracking-tight">Plan Your Career Path</h2>
+                    <div className="flex-1 h-[3px] bg-black"></div>
+                </div>
+                <div>
+                    <div className="flex justify-center items-center mt-25">
+                        <h2 className="bg-black/10 font-semibold text-5xl p-3 px-5 rounded-2xl">UI/UX Designer</h2>
+                    </div>
+                </div>
+        </section>
+    )
+}
+
+export default RoadmapSection;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/layout/Navbar"
 import HeroSection from "../features/landing/components/HeroSection"
 import AboutSection from "../features/landing/components/AboutSection"
+import RoadmapSection from "../features/landing/components/RoadmapSection"
 
 function LandingPage() {
     return(
@@ -8,6 +9,7 @@ function LandingPage() {
         <Navbar />
         <HeroSection />
         <AboutSection />
+        <RoadmapSection />
         </>
 
     )
