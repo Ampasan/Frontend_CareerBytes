02d0e2a086f6ba47876d6a14f1ef46a6413d5ac0
AIP-Code <aiprojectxd@gmail.com>
2026-04-24 19:22:28 +0700

    feat: add landing page

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,9 +1,11 @@
 function Button({text, variant = "primary", className = ""}) {
+    // OPSI VARIANT BUTTON
     const base = "p-3 px-13 font-semibold rounded-lg cursor-pointer transition";
     const variants = {
         primary: "bg-black text-white hover:bg-black/40",
         secondary: "bg-black/10 text-black hover:bg-black/20",
     };
+    
 
     return(
         <>
